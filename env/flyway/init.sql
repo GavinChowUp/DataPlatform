a9da99ef1f007888f8728e40b764236c8c027633
rong.han <1205395133@qq.com>
2022-08-08 23:38:57 +0800

    增加flyway

---
+++ env/flyway/init.sql
@@ -0,0 +1,12 @@
+grant all privileges on dbname.tablename to 'platform'@'%';
+grant all privileges on data_platform.* to 'platform'@'%';
+flush privileges;
+# use data_platform;
+CREATE TABLE IF NOT EXISTS `tbl`
+(
+    `id`       INT UNSIGNED AUTO_INCREMENT,
+    `title`    VARCHAR(100) NOT NULL,
+    `author`   VARCHAR(40)  NOT NULL,
+    `submission_date` DATE,
+    PRIMARY KEY (`id`)
+);
--- env/mysql/init/init.sql
+++ env/mysql/init/init.sql
@@ -1,12 +1,5 @@
-  grant all privileges on dbname.tablename to 'platform'@'%';
-  grant all privileges on data_platform.* to   'platform'@'%';
- flush privileges ;
-use data_platform;
-CREATE TABLE IF NOT EXISTS `tbl`
-(
-    `id`       INT UNSIGNED AUTO_INCREMENT,
-    `title`    VARCHAR(100) NOT NULL,
-    `author`   VARCHAR(40)  NOT NULL,
-    `submission_date` DATE,
-    PRIMARY KEY (`id`)
-);
+# grant all privileges on dbname.tablename to 'platform'@'%';
+grant all privileges on data_platform.* to 'platform'@'%';
+# 刷新权限
+flush privileges;
+
