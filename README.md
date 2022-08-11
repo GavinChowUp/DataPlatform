f14bc09a02df67439f19bab7dfb3fc9f95f6b8bd
rong.han <1205395133@qq.com>
2022-08-12 00:17:30 +0800

    调试成功sql初始化和flyway

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 DataPlatform
 
+# Reference
+flyway: https://flywaydb.org/documentation/concepts/migrations
+
 ## Development
 
 ### Requirements
@@ -15,6 +18,7 @@ DataPlatform
 cd project folder, then use:
 
 ```shell
+colima start -m 4 --mount /Volumes/Work/Code/DataPlatform/:w # 替换成自己的路径
 docker-compose up -d
 ```
 
--- env/flyway/sql/V2__Add_new_table.sql
+++ env/flyway/sql/V2__Add_new_table.sql
@@ -1,8 +1,5 @@
-# grant all privileges on dbname.tablename to 'platform'@'%';
-# grant all privileges on data_platform.* to 'platform'@'%';
-# flush privileges;
 use data_platform;
-CREATE TABLE IF NOT EXISTS `tbl`
+CREATE TABLE IF NOT EXISTS `tb2`
 (
     `id`       INT UNSIGNED AUTO_INCREMENT,
     `title`    VARCHAR(100) NOT NULL,
